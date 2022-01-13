60fd4276455a14e8a78ce1888f86e2dbe7d66ffa
Ga11eaN <manyukhy@gmail.com>
2022-01-13 11:04:21 +0200

    pre-release

--- upl/forms.py
+++ upl/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-choices = (('1', 'SimpleNN'),('2', 'Pretrained'))
+choices = (('1', 'Simple CNN'),('2', 'Pretrained NN'))
 class UploadDocumentForm(forms.Form):
     nn_id = forms.ChoiceField(choices=choices)
     image = forms.ImageField()
